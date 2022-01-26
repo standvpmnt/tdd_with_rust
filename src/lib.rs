b05ded5d3d341cc22ef2490c485ccabe7db79b51
standvpmnt <standvpmnt@gmail.com>
2022-01-26 20:41:54 +0530

    Setup a library project to try to implement tdd with rust following the TDD book by Kent Beck

---
+++ src/lib.rs
@@ -0,0 +1,30 @@
+struct Dollar {
+    amount: f32,
+}
+
+impl Dollar {
+    pub fn new(value: f32) -> Dollar {
+        Dollar { amount: value }
+    }
+
+    pub fn times(&mut self, multiplier: f32) {
+        self.amount = self.amount * multiplier;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+    #[test]
+    fn testMultiplication() {
+        let mut five = Dollar::new(5.0);
+        five.times(2.0);
+        assert_eq!(10.0, five.amount)
+    }
+}
