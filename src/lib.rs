5e1a575a2b85ae8386b37512d14811c94466793e
standvpmnt <standvpmnt@gmail.com>
2022-02-16 18:47:35 +0530

    Chapter 11 Removing structs which were not being used and move all testing to be around the Money struct instead

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,5 @@
 #![allow(dead_code)]
 
-struct Dollar;
-
 struct Money {
     amount: f32,
     currency: String,
@@ -24,13 +22,10 @@ impl Money {
         let temp = &self.currency.to_owned();
         temp.to_owned()
     }
-}
-
-impl Dollar {
-    pub fn new(value: f32) -> Money {
+    fn new(value: f32, currency: &str) -> Money {
         Money {
             amount: value,
-            currency: "USD".to_owned(),
+            currency: currency.to_owned(),
         }
     }
 }
@@ -47,61 +42,39 @@ impl ToString for Money {
     }
 }
 
-struct Franc;
-
-impl Franc {
-    pub fn new(value: f32) -> Money {
-        Money {
-            amount: value,
-            currency: "CHF".to_owned(),
-        }
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn test_multiplication() {
-        let five = Dollar::new(5.0);
-        assert!(Dollar::new(10.0) == five.times(2.0));
-        assert!(Dollar::new(15.0).equals(&five.times(3.0)));
+        let five = Money::new(5.0, "USD");
+        assert!(Money::new(10.0, "USD") == five.times(2.0));
+        assert!(Money::new(15.0, "USD").equals(&five.times(3.0)));
     }
 
     #[test]
     fn test_equality() {
-        assert!(Dollar::new(5.0).equals(&Dollar::new(5.0)));
-        assert!(!Dollar::new(5.0).equals(&Dollar::new(6.0)));
-        assert!(Franc::new(5.0).equals(&Franc::new(5.0)));
-        assert!(!Franc::new(5.0).equals(&Franc::new(6.0)));
-        // assert!(
-        //     !Franc::new(5.0).equals(&Dollar::new(5.0)),
-        //     "Dollar and franc are not equal"
-        // );
+        assert!(Money::new(5.0, "USD").equals(&Money::new(5.0, "USD")));
+        assert!(!Money::new(5.0, "USD").equals(&Money::new(6.0, "USD")));
+        assert!(Money::new(5.0, "CHF").equals(&Money::new(5.0, "CHF")));
     }
 
     #[test]
     fn test_trait_based_equality() {
-        assert!(Dollar::new(5.0) == Dollar::new(5.0));
-        assert!(Dollar::new(5.0) != Dollar::new(6.0));
-        assert!(Dollar::new(5.0) != Franc::new(5.0));
-    }
-    #[test]
-    fn test_franc_multiplication() {
-        let five = Franc::new(5.0);
-        assert!(Franc::new(10.0).equals(&five.times(2.0)));
-        assert!(Franc::new(15.0).equals(&five.times(3.0)));
+        assert!(Money::new(5.0, "USD") == Money::new(5.0, "USD"));
+        assert!(Money::new(5.0, "USD") != Money::new(6.0, "USD"));
+        assert!(Money::new(5.0, "CHF") != Money::new(5.0, "USD"));
     }
 
     #[test]
     fn test_currency() {
-        assert_eq!("USD", Dollar::new(5.0).currency());
-        assert_eq!("CHF", Franc::new(5.0).currency());
+        assert_eq!("USD", Money::new(5.0, "USD").currency());
+        assert_eq!("CHF", Money::new(5.0, "CHF").currency());
     }
 
     #[test]
     fn test_to_string() {
-        assert_eq!("5 USD", Dollar::new(5.0).to_string());
+        assert_eq!("5 USD", Money::new(5.0, "USD").to_string());
     }
 }
